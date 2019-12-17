9e85e11c2476f84069a7d5e4b9297ca8a830120d
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2019-12-17 12:31:05 +0000

    write to google sheet

--- app/routes.js
+++ app/routes.js
@@ -94,8 +94,8 @@ router.get('/sheet2', function (req, res) {
 
 	  // Get all of the rows from the spreadsheet.
 	  doc.getRows(1, function (err, rows) {
-	  	sessionDate = rows[0]['date-saved'];
-	  	x = JSON.parse(rows[0]['session-json']); 
+	  	sessionDate = rows[0]['date_saved'];
+	  	x = JSON.parse(rows[0]['session_json']); 
 	  	//	var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');		
 			// x = rows[1]['session-json']; 
 	    // console.log(rowData);
@@ -104,6 +104,95 @@ router.get('/sheet2', function (req, res) {
 
 	req.session.data = x;
   res.render('sheet2', { 'sheetData': sessionDate })
+})
+
+
+router.get('/docs/examples/pass-data/vehicle-registration-car1-sheet', function (req, res) {
+	var GoogleSpreadsheet = require('google-spreadsheet');
+	var creds = require('./client_secret.json');
+
+	// Create a document object using the ID of the spreadsheet - obtained from its URL.
+	var doc = new GoogleSpreadsheet('1fFt4Xp8eE2Ii_a2YPqJnG7iN9OvNO4qWt0kJDUxJfvk');
+
+	// Authenticate with the Google Spreadsheets API.
+	doc.useServiceAccountAuth(creds, function (err) {
+
+	x = "";
+
+	  // Get named cell from row 2 of the spreadsheet.
+	  doc.getRows(1, function (err, rows) {
+	  	x = JSON.parse(rows[0]['session_json']); 
+	  });
+	});
+
+	req.session.data = x;
+
+  res.redirect('vehicle-registration')
+})
+
+router.get('/docs/examples/pass-data/vehicle-registration-lorry1-sheet', function (req, res) {
+	var GoogleSpreadsheet = require('google-spreadsheet');
+	var creds = require('./client_secret.json');
+
+	// Create a document object using the ID of the spreadsheet - obtained from its URL.
+	var doc = new GoogleSpreadsheet('1fFt4Xp8eE2Ii_a2YPqJnG7iN9OvNO4qWt0kJDUxJfvk');
+
+	// Authenticate with the Google Spreadsheets API.
+	doc.useServiceAccountAuth(creds, function (err) {
+
+	x = "";
+
+	  // Get named cell from row 2 of the spreadsheet.
+	  doc.getRows(1, function (err, rows) {
+	  	x = JSON.parse(rows[1]['session_json']); 
+	  });
+	});
+
+	req.session.data = x;
+
+  res.redirect('vehicle-registration')
+})
+
+
+router.get('/save-data', function (req, res) {
+	// route to take session data, store it into google sheet in a new row, return the row number 
+	// render saved data page and pass in the row number
+	var GoogleSpreadsheet = require('google-spreadsheet');
+	var creds = require('./client_secret.json');
+
+	var rowNum;
+
+	// Create a document object using the ID of the spreadsheet - obtained from its URL.
+	var doc = new GoogleSpreadsheet('1fFt4Xp8eE2Ii_a2YPqJnG7iN9OvNO4qWt0kJDUxJfvk');
+
+	// Authenticate with the Google Spreadsheets API.
+	doc.useServiceAccountAuth(creds, function (err) {
+
+		// get session data as a string
+		x = "";
+		x = JSON.stringify(req.session.data);
+
+		// get todays date and time as a string
+		var d = new Date();
+  	var n = d.toISOString();
+
+
+		// save session oject and todays date/time - as a string
+		doc.addRow(1, { session_json: x, date_saved: n }, function(err) {
+		  if(err) {
+		    console.log(err);
+		  }
+		});
+
+		// get row count (to find out which row we've added - assuming it adds to bottom of sheet)
+		doc.getRows(1, function (err, rows) {
+	  	rowNum = rows.length;
+	  });
+		
+
+	});
+
+  res.render('saved-data', { 'rowNumber': rowNum })
 })
 
 
